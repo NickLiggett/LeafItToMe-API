cf13d76728c78614b086990369745123bb13a1d9
Nick Liggett <nick.liggett@gmail.com>
2023-01-29 15:54:54 -0700

    Configure Endpoints

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const express = require("express");
-const members = require("./members");
+const customers = require("./customers");
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (request, response) => {
-  response.status(200).json({ members: members });
+app.get("/customers", (request, response) => {
+  response.status(200).json({ customers: customers });
 });
 
-app.post("/", (request, response) => {
-  const newMember = {
+app.get("/customers/:id", (request, response) => {
+  const singleUser = customers.find(
+    (customer) => customer.id === parseInt(request.params.id)
+  );
+  response.status(200).json({ customer: singleUser });
+});
+
+app.post("/add", (request, response) => {
+  const newCustomer = {
     id: request.body.id,
-    name: request.body.name,
-    age: request.body.age,
+    first_name: request.body.first_name,
+    last_name: request.body.last_name,
+    username: request.body.username,
+    password: request.body.password,
+    city: request.body.city,
+    state: request.body.state,
+    zip_code: request.body.zip_code,
+    user_img: request.body.user_img,
+    plants: [
+      {
+        plant_id: request.body.plant_id,
+        species: request.body.species,
+        img: request.body.img,
+        instructions: request.body.instructions,
+      },
+    ],
   };
-  members.push(newMember);
-  response.status(200).json({ members: members });
+  customers.push(newCustomer);
+  response.status(200).json({ customers: customers });
 });
 
 app.delete("/:id", (request, response) => {
-  const filteredMembers = members.filter(
-    (member) => member.id !== parseInt(request.params.id)
+  const filteredCustomers = customers.filter(
+    (customer) => customer.id !== parseInt(request.params.id)
   );
-  response.status(200).json({ members: filteredMembers });
+  response.status(200).json({ customers: filteredCustomers });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
