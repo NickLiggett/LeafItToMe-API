2ef0da61ea28650e58b8084bab78e9344ebf72a4
Nick Liggett <nick.liggett@gmail.com>
2023-02-03 10:49:51 -0700

    Attempt Delete Function

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/customers/:id", (request, response) => {
   response.status(200).json({ customer: singleUser });
 });
 
-app.post("/add", (request, response) => {
+app.post("/customers/add", (request, response) => {
   const newCustomer = {
     id: request.body.id,
     first_name: request.body.first_name,
@@ -28,17 +28,24 @@ app.post("/add", (request, response) => {
     state: request.body.state,
     zip_code: request.body.zip_code,
     user_img: request.body.user_img,
-    plants: [
-      {
-        plant_id: request.body.plant_id,
-        species: request.body.species,
-        img: request.body.img,
-        instructions: request.body.instructions,
-      },
-    ],
+    plants: [],
   };
   customers.push(newCustomer);
   response.status(200).json({ customers: customers });
+});
+
+app.post("/customer/addPlant/:id", (request, response) => {
+  const newPlant = {
+    plant_id: request.body.plant_id,
+    species: request.body.species,
+    img: request.body.img,
+    instructions: request.body.instructions,
+  };
+  const theCustomer = customers.find(
+    (customer) => customer.id === parseInt(request.params.id)
+  );
+  theCustomer.plants.push(newPlant);
+  response.status(200).json({ plants: theCustomer.plants });
 });
 
 app.delete("/:id", (request, response) => {
@@ -48,6 +55,17 @@ app.delete("/:id", (request, response) => {
   response.status(200).json({ customers: filteredCustomers });
 });
 
+app.delete("/customer/:id/plant/:plantID", (request, response) => {
+  const theCustomer = customers.find(
+    (customer) => customer.id === parseInt(request.params.id)
+  );
+  const index = theCustomer.plants.findIndex(
+    (plant) => plant.id === request.params.plantID
+  );
+  theCustomer.plants.splice(index, 1);
+  response.status(200).json({ plants: theCustomer.plants });
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app
+module.exports = app;
